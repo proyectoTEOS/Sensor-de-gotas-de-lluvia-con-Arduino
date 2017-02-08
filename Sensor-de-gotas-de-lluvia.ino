915a68839081aa83871fc206115633968544a155
proyectoTEOS <proyectoteos@gmail.com>
2017-02-08 00:20:39 -0600

    Diagram & Code
    Diagram of connection between sensor and Arduino

---
+++ Sensor-de-gotas-de-lluvia.ino
@@ -0,0 +1,19 @@
+const int sensorPinT = A0;
+
+int valT;
+int finalvalT;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  valT = map(analogRead(sensorPinT), 0, 1023, 95, 0);
+  finalvalT = constrain (valT, 0, 95);
+  Serial.print("Hay un ");
+  Serial.print(finalvalT);
+  Serial.println("% de gotas de agua"); 
+  delay(100);
+}
+
+
