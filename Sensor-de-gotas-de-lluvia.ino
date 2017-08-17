0938c52a2191cf48b0b983076e1908cb50efef86
proyectoTEOS <proyectoteos@gmail.com>
2017-08-17 10:24:36 -0500

    Code modification
    Code optimized at runtime

--- Sensor-de-gotas-de-lluvia.ino
+++ Sensor-de-gotas-de-lluvia.ino
@@ -1,19 +1,31 @@
+/*
+  Created by TEOS
+
+  Domotic with Arduino https://goo.gl/btZpjs
+
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+  WEB http://bit.ly/1UvumG9
+*/
+
 const int sensorPinT = A0;
 
 int valT;
 int finalvalT;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
   valT = map(analogRead(sensorPinT), 0, 1023, 95, 0);
   finalvalT = constrain (valT, 0, 95);
   Serial.print("Hay un ");
   Serial.print(finalvalT);
-  Serial.println("% de gotas de agua"); 
+  Serial.println("% de gotas de agua");
   delay(100);
 }
-
-
