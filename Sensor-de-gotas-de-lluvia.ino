eafb2908d0cda3603f4bebc4f9ca08cfb37c0c0e
TEOS <proyectoteos@gmail.com>
2018-01-12 17:29:40 -0600

    Code update
    Improvement of the sensor reading

--- Sensor-de-gotas-de-lluvia.ino
+++ Sensor-de-gotas-de-lluvia.ino
@@ -15,17 +15,16 @@
 const int sensorPinT = A0;
 
 int valT;
-int finalvalT;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  valT = map(analogRead(sensorPinT), 0, 1023, 95, 0);
-  finalvalT = constrain (valT, 0, 95);
+  valT = map(analogRead(sensorPinT), 0, 1023, 100, 0);
+  valT = constrain (valT, 0, 100);
   Serial.print("Hay un ");
-  Serial.print(finalvalT);
+  Serial.print(valT);
   Serial.println("% de gotas de agua");
   delay(100);
 }
